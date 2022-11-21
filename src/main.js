4c5fa8c1406bca8dc76dd1938345e624368118a5
Phil Selker <26040772+pselker2@users.noreply.github.com>
2022-11-21 16:01:51 -0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,31 @@
+
+function setup() {
+
+  Alpine.store('model', {
+    currentPage: 'home', // 'home', 'service'
+    currentLanguage: 'english',
+    get page() {
+      return this.currentPage;
+    },
+    set page(nextPage) {
+      this.currentPage = nextPage;
+    },
+    currentLanguage: 'english',
+    languages: ['english', 'norwegian'],
+    get language() {
+      return this.currentLanguage;
+    },
+    set language(current) {
+      this.currentLanguage = current;
+    },
+    services: [
+      { url: 'pselker@cisco.call.ciscospark.com', name: 'Loan' },
+      { url: 'luvelasc@cisco.call.ciscospark.com', name: 'Advice' },
+      { url: 'erica.talking@ivr.vc', name: 'Credit' },
+    ],
+  });
+
+}
+
+document.addEventListener('alpine:init', setup);
+
